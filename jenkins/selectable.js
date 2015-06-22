1119083caf62391558b086d95f1571fe8c7aac82
Jiri Stefek <jstefek@redhat.com>
2015-06-22 15:00:10 +0200

    initial commit

---
+++ jenkins/selectable.js
@@ -0,0 +1,20 @@
+function selectable(className, $input, appendable){
+    var elems= jQuery('.'+className);
+    elems.each(function (i,e){
+        var $e= jQuery(e);
+        $e.on('click', function(evt){
+            var toSet=jQuery(this).text();
+            if(appendable){
+                if(evt.shiftKey || evt.ctrlKey){
+                    var valueBefore=$input.val();
+                    if(valueBefore){
+                        toSet=valueBefore+',' + toSet;
+                    }
+                }
+            }
+            $input.val(toSet);
+        });
+        $e.css('color', 'blue');
+        $e.css('cursor', 'Pointer');
+    });
+}
